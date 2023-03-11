ac758d14ff7930d811747b39be71f4bf69d9af7e
Niku Grönberg <gronbergniku@gmail.com>
2023-03-11 18:06:26 +0200

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['dist/*', 'node_modules/*'],
+  root: true,
+}
