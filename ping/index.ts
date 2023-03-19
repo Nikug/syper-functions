32430bc4799010a61bd723117c409b491fed5cd2
Niku Grönberg <gronbergniku@gmail.com>
2023-03-19 15:12:07 +0200

    change pang to pong

--- ping/index.ts
+++ ping/index.ts
@@ -5,14 +5,10 @@ const httpTrigger: AzureFunction = async function (
   req: HttpRequest
 ): Promise<void> {
   context.log('HTTP trigger function processed a request.')
-  const name = req.query.name || (req.body && req.body.name)
-  // const responseMessage = name
-  //   ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-  //   : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
   context.res = {
     status: 200 /* Defaults to 200 */,
-    body: 'pang!',
+    body: 'pong!',
   }
 }
 
