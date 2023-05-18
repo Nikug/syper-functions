bd5fbe2675943d2e4d6c9dd55d9b14e66f76db35
Niku Grönberg <gronbergniku@gmail.com>
2023-05-18 18:41:18 +0300

    use azure credentials

--- cosmos.ts
+++ cosmos.ts
@@ -1,10 +1,13 @@
 import { Container, CosmosClient, Database } from '@azure/cosmos'
+import { DefaultAzureCredential } from '@azure/identity'
 
 const endpoint = process.env.cosmosDb__accountEndpoint
-const key = process.env.cosmosDb__key
 const databaseName = process.env.CosmosDbName
+const credential = new DefaultAzureCredential({
+  managedIdentityClientId: process.env.cosmosDb__clientId,
+})
 
-const cosmosClient = new CosmosClient({ endpoint, key })
+const cosmosClient = new CosmosClient({ endpoint, aadCredentials: credential })
 
 const getDatabase = async (): Promise<Database> => {
   const { database } = await cosmosClient.databases.createIfNotExists({ id: databaseName })
